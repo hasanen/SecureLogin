5fa93b42d6649773d5230413d08df65630241a9b
Joni Hasanen <joni.hasanen@gmail.com>
2013-04-16 20:28:08 +0300

    User cannot now login with correct credentialUser cannot now login with correct credentialss

--- event.Core.LoginPost.php
+++ event.Core.LoginPost.php
@@ -1,4 +1,9 @@
 <?php
 if( !isset($gCms) ) exit;
+global $config;
 
 $continueLogin = $this->secureLogin()->userIsAllowedToLogin($params['user']->username, $_SERVER['SERVER_ADDR']);
+if(!$continueLogin){ 
+  $_SESSION['logout_user_now'] = true;
+  header('Location: ' . $config['admin_url'] . '/login.php');
+}
