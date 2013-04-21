17f8e2fe583a7167a0bd4c6c669b213adff330a0
Joni Hasanen <joni.hasanen@gmail.com>
2013-04-21 20:25:01 +0300

    Refactored and added few methods

--- lib/SecureLogin.class.php
+++ lib/SecureLogin.class.php
@@ -10,17 +10,48 @@ class SecureLogin{
     $this->_whitelistTable = $tablePrefix . '_whitelist';
   }
 
+  private function getRowByUsernameAndIp($username, $ip, $validated = 1){
+    $query = 'SELECT id, validateWithKey, username, ip FROM ' . $this->_whitelistTable . ' WHERE username = ? AND ip = ? AND validated = ?';
+    $result = $this->_db->Execute($query, array($username, $ip, $validated));  
+
+    
+    if($result){
+      return $result->FetchRow();
+    } else {
+      return null;
+    }
+  }
+
   public function userIsAllowedToLogin($username, $ip){
-    $query = sprintf('SELECT id FROM %s WHERE username = ? AND ip = ? AND validated = 1', $this->_whitelistTable);
-    $result = $this->_db->Execute($query, array($username, $ip));
-    if($result && $result->FetchRow()){
+    if($this->getRowByUsernameAndIp($username, $ip) != null){
       return true;
     } else {
       return false;
     }
   }
 
-  public function createValidationUrl($siteroot, $username, $ip){
+  public function createValidationKey($username, $ip){
+    $row = $this->getRowByUsernameAndIp($username, $ip, 0);
+    if($row != null){
+      $validationKey = $row['validateWithKey'];
+    } else {
+      $validationKey = md5(time() . $username . $ip . rand());
+      $id = $this->_db->GenID($this->_whitelistTable . '_seq');
 
+      $query = 'INSERT INTO ' . $this->_whitelistTable . ' (id, username, ip, validateWithKey) VALUES (?, ?, ?, ?)';
+      $this->_db->Execute($query, array($id, $username, $ip, $validationKey));
+    }
+    return $validationKey;
+  }
+
+  public function getAllIps(){
+    $entries = array();
+        
+    $query = 'SELECT id, username, ip, for_all, validated  FROM '. $this->_whitelistTable . ' ORDER BY username ASC';
+    $result = $this->_db->Execute($query);
+    while($result && $row = $result->FetchRow()){
+      array_push($entries, $row);
+    }
+    return $entries;
   }
 }
